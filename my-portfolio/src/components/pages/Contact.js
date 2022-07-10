3377027e02c02591356a28a90415edc394962d8d
r134x7 <r134x7@gmx.com>
2022-07-11 00:25:19 +1000

    fixed bugs and implemented correctly validation

--- my-portfolio/src/components/pages/Contact.js
+++ my-portfolio/src/components/pages/Contact.js
@@ -4,60 +4,95 @@ import { useForm } from '@mantine/form';
 import { useClickOutside } from '@mantine/hooks';
 import { useState } from 'react';
 
-const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ; // taken from 16-Stu_React-Forms utils/helpers.js
+const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ; // regex taken from 16-Stu_React-Forms utils/helpers.js
+
+// this form has two validate functions, one when clicking outside a text field and the other when submitting.
 
 function Contact() {
-  const form = useForm({
-    initialValues: {
+  const form = useForm({ // useForm is a Mantine function
+    initialValues: { // objects for the fields you are using
         name: '',
         email: '',
         message: '',
     },
 
-    validate: {
+    validate: { // validate function that occurs on Submit.
         name: (value) => (value.length < 2 ? 'Name must have at least 2 letters.' : null),
         email: (value) => (emailValidation.test(value) ? null : 'Invalid email'),
         message: (value) => (value.length === 0 ? 'Message is required.' : null),
     },
   });
 
+const [nameOpened, setNameOpened] = useState(false) // flag to check if Name text field was clicked on
+const [emailOpened, setEmailOpened] = useState(false) // flag to check if Email text field was clicked on
+const [messageOpened, setMessageOpened] = useState(false) // flag to check if Message text field was clicked on
 
-const [emailError, setEmailError] = useState("");
+const [nameError, setNameError] = useState(""); // error state when clicking outside text field
+const [emailError, setEmailError] = useState(""); // error state when clicking outside text field
+const [messageError, setMessageError] = useState(""); // error state when clicking outside text field
 
-  const emailref = useClickOutside(() => {
-    if (!emailValidation.test(form.getInputProps("email").value)) {
-        setEmailError("Invalid Email");
-    } else {
-        setEmailError("");
+const nameRef = useClickOutside(() => { // Mantine function
+    if (form.getInputProps("name").value.length < 2 && nameOpened) { // uses name input from form, checks if nameOpened is true
+        setNameError("Name must have at least 2 letters."); // error displays in span
+        setNameOpened(false);
+    } else if (nameOpened) {
+        setNameError(""); // if error occurred, it will be removed on correct input
+        setNameOpened(false);
     }
 });
 
-  return (
+const emailRef = useClickOutside(() => { // Mantine function
+    if (!emailValidation.test(form.getInputProps("email").value) && emailOpened) { // uses email input from form and does validation check, checks if nameOpened is true
+        setEmailError("Invalid email."); // error displays in span
+        setEmailOpened(false);
+    } else if (emailOpened) {
+        setEmailError(""); // if error occurred, it will be removed on correct input
+        setEmailOpened(false);
+    }
+});
+
+const messageRef = useClickOutside(() => {
+    if (form.getInputProps("message").value.length === 0 && messageOpened) {
+        setMessageError("Message is required."); // error displays in span
+        setMessageOpened(false);
+    } else if (messageOpened) {
+        setMessageError(""); // if error occurred, it will be removed on correct input
+        setMessageOpened(false);
+    }
+});
+
+  return ( // Box, TextInput, Textarea, Group and Button are Mantine properties
     <Box sx={{ maxWidth: 300 }} mx="auto">
       <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput
-            required
+        <TextInput //name field
+            onClick={() => setNameOpened(true)} // changes state when clicked
+            ref={nameRef}
+            required // requires entry
             label="Name"
             placeholder="Your Name"
-            {...form.getInputProps('name')}
+            {...form.getInputProps('name')} // uses name input on submit
         />
 
-        <TextInput
-            ref={emailref}
-            required
+        <TextInput // email field
+            onClick={() => setEmailOpened(true)} // changes state when clicked
+            ref={emailRef}
+            required // requires entry
             label="Email"
             placeholder="your@email.com"
-            {...form.getInputProps('email')}
+            {...form.getInputProps('email')} // uses email input on submit
         />
 
-        <Textarea
-            autosize
-            required
+        <Textarea // message field
+            onClick={() => setMessageOpened(true)} // changes state when clicked
+            ref={messageRef}
+            autosize // text box will get larger as text is entered
+            required // requires entry
             label="Message"
-            {...form.getInputProps('message')}
+            {...form.getInputProps('message')} // uses message input on submit
         />
 
-        <span>{emailError}</span>
+        {/* where the outside click errors occur */}
+        <span> {`${nameError} ${emailError} ${messageError}`}</span>
 
         <Group position="left" mt="md">
           <Button type="submit">Submit</Button>
@@ -68,15 +103,3 @@ const [emailError, setEmailError] = useState("");
 }
 
 export default Contact;
-
-
-// export default function Contact() {
-//   return (
-//     <div>
-//       <h1>Contact me</h1>
-//       <p>
-//         Testing...
-//       </p>
-//     </div>
-//   );
-// }
