0cca3116966aa9a7f508c4e3501ca3313fb13ace
r134x7 <r134x7@gmx.com>
2022-07-10 20:52:50 +1000

    finally created a functioning error handler when clicking

--- my-portfolio/src/components/pages/Contact.js
+++ my-portfolio/src/components/pages/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { TextInput, Textarea, Button, Group, Box } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { useClickOutside } from '@mantine/hooks';
+import { useState } from 'react';
+
+const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ; // taken from 16-Stu_React-Forms utils/helpers.js
 
 function Contact() {
   const form = useForm({
@@ -12,10 +16,21 @@ function Contact() {
 
     validate: {
         name: (value) => (value.length < 2 ? 'Name must have at least 2 letters.' : null),
-        email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+        email: (value) => (emailValidation.test(value) ? null : 'Invalid email'),
         message: (value) => (value.length === 0 ? 'Message is required.' : null),
     },
   });
+
+
+const [emailError, setEmailError] = useState("");
+
+  const emailref = useClickOutside(() => {
+    if (!emailValidation.test(form.getInputProps("email").value)) {
+        setEmailError("Invalid Email");
+    } else {
+        setEmailError("");
+    }
+});
 
   return (
     <Box sx={{ maxWidth: 300 }} mx="auto">
@@ -28,6 +43,7 @@ function Contact() {
         />
 
         <TextInput
+            ref={emailref}
             required
             label="Email"
             placeholder="your@email.com"
@@ -40,6 +56,8 @@ function Contact() {
             label="Message"
             {...form.getInputProps('message')}
         />
+
+        <span>{emailError}</span>
 
         <Group position="left" mt="md">
           <Button type="submit">Submit</Button>
