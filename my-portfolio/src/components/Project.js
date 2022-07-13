7746ff462583a60d342463c3c713d0ed71dd21c3
r134x7 <r134x7@gmx.com>
2022-07-13 16:43:33 +1000

    updated project with more data needed, portfolio now has cards

--- my-portfolio/src/components/Project.js
+++ my-portfolio/src/components/Project.js
@@ -13,42 +13,73 @@ const works = [
         name: "Nintendo Earnings Data and Other Video Game Companies",
         deployedApp: "https://github.com/r134x7/nintendo-earnings-data-and-other-video-game-companies",
         githubRepo: "https://github.com/r134x7/nintendo-earnings-data-and-other-video-game-companies",
-        image: my1
+        image: my1,
+        badge: "Python",
+        badgeColour: "blue",
+        buttonAppText: "There is no Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Calculates and outputs code format data."
     },
     {
         id: 2,
         name: "RunningTracks",
         deployedApp: "https://quiet-reaches-87273.herokuapp.com/",
         githubRepo: "https://github.com/r134x7/RunningTracks",
-        image: my2
+        image: my2,
+        badge: "JavaScript",
+        badgeColour: "yellow",
+        buttonAppText: "Click here to Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Fullstack app, records user's running data and makes charts of it."
     },
     {
         id: 3,
         name: "PWA JavaScript Text Editor",
         deployedApp: "https://morning-chamber-10447.herokuapp.com/",
         githubRepo: "https://github.com/r134x7/pwa-javascript-text-editor",
-        image: my3
+        image: my3,
+        badge: "JavaScript",
+        badgeColour: "yellow",
+        buttonAppText: "Click here to Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Fullstack progressive web app, text editor functions offline."
+
     },
     {
         id: 4,
         name: "Milktoast TechBlog",
         deployedApp: "https://immense-brushlands-18787.herokuapp.com/",
         githubRepo: "https://github.com/r134x7/milktoast-techBlog",
-        image: my4
+        image: my4,
+        badge: "JavaScript",
+        badgeColour: "yellow",
+        buttonAppText: "Click here to Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Fullstack app, simple blog posts and comment functions."
     },
     {
         id: 5,
         name: "Weather Dashboard",
         deployedApp: "https://r134x7.github.io/HW06-ServerSideAPIs-Weather-Dashboard/",
         githubRepo: "https://github.com/r134x7/HW06-ServerSideAPIs-Weather-Dashboard",
-        image: my5
+        image: my5,
+        badge: "JavaScript",
+        badgeColour: "yellow",
+        buttonAppText: "Click here to Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Front-end app, uses API calls to third party to display weather data."
     },
     {
         id: 6,
         name: "Password Generator",
         deployedApp: "https://r134x7.github.io/HW03-JavaScript-Password-Generator/",
         githubRepo: "https://github.com/r134x7/HW03-JavaScript-Password-Generator",
-        image: my6
+        image: my6,
+        badge: "JavaScript",
+        badgeColour: "yellow",
+        buttonAppText: "Click here to Deployed App",
+        buttonRepoText: "Click here to GitHub Repo",
+        textDescription: "Front-end app, uses functions to generate a password."
     },
 ]
 
--- my-portfolio/src/components/pages/Portfolio.js
+++ my-portfolio/src/components/pages/Portfolio.js
@@ -1,22 +1,68 @@
 import React from 'react';
-import { SimpleGrid } from "@mantine/core"
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme, SimpleGrid } from '@mantine/core';
 
 export default function Portfolio(props) {
+  const theme = useMantineTheme();
+
+  const secondaryColor = theme.colorScheme === 'dark'
+    ? theme.colors.dark[1]
+    : theme.colors.gray[7];
+
   return (
     <SimpleGrid cols={2}
     breakpoints={[
-      { maxWidth: 820, cols: 1, spacing: 'sm' },
+      { maxWidth: 800, cols: 1, spacing: 'sm' },
     ]}
     >
-      {props.works.map(item => 
-      <div key={item.id}>
-        <img src={item.image} alt={`Project ${item.id}`} />
-      </div>
+    {props.works.map(item => 
+
+      <div style={{ width: 360, margin: 'auto' }} key={item.id}>
+      <Card shadow="sm" p="lg">
+        <Card.Section>
+          <Image height={220} src={item.image} alt={`Project ${item.id}`} />
+        </Card.Section>
+
+        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+          <Text weight={500}>{item.name}</Text>
+          <Badge color={item.badgeColour} variant="light">
+            {item.badge}
+          </Badge>
+        </Group>
+
+        <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
+        {item.textDescription}
+        </Text>
+
+        <Button component="a" href={item.deployedApp} variant="light" color="cyan" fullWidth style={{ marginTop: 14 }}>
+          {item.buttonAppText}
+        </Button>
+        <Button component="a" href={item.githubRepo} variant="light" color="green" fullWidth style={{ marginTop: 14 }}>
+        {item.buttonRepoText}
+        </Button>
+      </Card>
+    </div>
       )
-      }
+    }
     </SimpleGrid>
   );
 }
+
+// export default function Portfolio(props) {
+//   return (
+//     <SimpleGrid cols={2}
+//     breakpoints={[
+//       { maxWidth: 820, cols: 1, spacing: 'sm' },
+//     ]}
+//     >
+//       {props.works.map(item => 
+//       <div key={item.id}>
+//         <img src={item.image} alt={`Project ${item.id}`} />
+//       </div>
+//       )
+//       }
+//     </SimpleGrid>
+//   );
+// }
 
 // export default function Portfolio(props) {
 //   return (
