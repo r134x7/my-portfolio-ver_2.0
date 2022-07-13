80b5e309aa6c6df829a53394e0c7b1ac642fc9d3
r134x7 <r134x7@gmx.com>
2022-07-13 19:04:47 +1000

    updated About page structure

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { Avatar } from "@mantine/core";
+import { Avatar, Box, Space } from "@mantine/core";
 import c1b from "../../assets/images/c1b.png";
-
-/* <img src={c1b} alt="Avatar" /> */
 
 export default function About() {
     return (
-        <div>
+        <Box sx={(theme) => ({
+            // maxWidth: '300px',
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            alignContent: "center",
+            alignItems: "baseline",
+            padding: "2em",
+            '@media (max-width: 768px)': {
+               paddingLeft: "10em",
+               paddingRight: "10em",
+            },
+            '@media (max-width: 600px)': {
+                padding: "2em",
+               flexDirection: "row"
+             },
+             h1: {
+                paddingRight: "1em",
+             }
+        })}>
             <h1>About me</h1>
-
+            <Space h="md" />
             <Avatar src={c1b} alt="Avatar" radius="lg" size="xl" />
-            
+            <Space h="md" />
             <p>
             I am Github user r134x7 also known as ggx2ac in some places, I am currently
-studying full-stack web development via coding bootcamp to gain programming skills
-and to build my portfolio so that I can find a career in web development.
-My educational background is a Bachelor of Science with a double major
-in Physics and, Financial Mathematics and Statistics.
-
-My programming experience during University consisted of:
-learning Matlab for scientific applications and R language for
-applying statistics. After University, I learned Java (self-taught)
-to display finacial data in code printed format. I then taught myself
-Python to build new code for that same data because the Java IDE mobile
-app I was using no longer worked and there was no satisfactory replacement.
-To contact me for my resume, go to my Github profile for my contact details
-which is found in the "contact me" section below.
+            studying full-stack web development via coding bootcamp to gain programming skills
+            and to build my portfolio so that I can find a career in web development.
+            My educational background is a Bachelor of Science with a double major
+            in Physics and, Financial Mathematics and Statistics.
             </p>
-        </div>
+
+<Space h="md" />
+ 
+            <p>
+            My programming experience during University consisted of:
+            learning Matlab for scientific applications and R language for
+            applying statistics. After University, I learned Java (self-taught)
+            to display finacial data in code printed format. I then taught myself
+            Python to build new code for that same data because the Java IDE mobile
+            app I was using no longer worked and there was no satisfactory replacement.
+            To contact me for my resume, go to my Github profile for my contact details
+            which is found in the "contact me" section below.
+            </p>
+        </Box>
     )
 }
