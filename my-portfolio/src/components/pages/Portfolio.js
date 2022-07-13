cb8d6c819522edf5f7816b1d18277fb3324caed4
r134x7 <r134x7@gmx.com>
2022-07-13 14:36:41 +1000

    added grid to portfolio

--- my-portfolio/src/components/pages/Portfolio.js
+++ my-portfolio/src/components/pages/Portfolio.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { SimpleGrid } from "@mantine/core"
 
 export default function Portfolio(props) {
   return (
-    <div>
-      <ul>
-        {props.works.map(item => (
-          <li key={item.id}>
-            {item.name}
-            {item.deployedApp}
-            {item.githubRepo}
-            <img src={item.image} alt={`Project ${item.id}`} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <SimpleGrid cols={2}
+    breakpoints={[
+      { maxWidth: 820, cols: 1, spacing: 'sm' },
+    ]}
+    >
+      {props.works.map(item => 
+      <div key={item.id}>
+        <img src={item.image} alt={`Project ${item.id}`} />
+      </div>
+      )
+      }
+    </SimpleGrid>
   );
 }
+
+// export default function Portfolio(props) {
+//   return (
+//     <div>
+//       <ul>
+//         {props.works.map(item => (
+//           <li key={item.id}>
+//             {item.name}
+//             {item.deployedApp}
+//             {item.githubRepo}
+//             <img src={item.image} alt={`Project ${item.id}`} />
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
