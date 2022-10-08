2db022f480dfe49a43d289875911f8eb03c6cf18
r134x7 <r134x7@gmx.com>
2022-10-08 16:11:21 +1100

    added mantine color theme to app

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,32 +1,33 @@
-import React, { useState } from "react";
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import Header from "./header";
-import Footer from "./footer";
+// import React, { useState } from "react";
+// import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+// import Header from "./header";
+// import Footer from "./footer";
 
-export default function Layout({ children}: {children: React.ReactNode}) {
+// export default function Layout({ children}: {children: React.ReactNode}) {
 
-    const [colorScheme, setColorScheme] = useState<ColorScheme>('dark'); // when pressing a specific icon it toggles the light/dark mode
-    const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+//     const [colorScheme, setColorScheme] = useState<ColorScheme>('dark'); // when pressing a specific icon it toggles the light/dark mode
+//     const toggleColorScheme = (value?: ColorScheme) =>
+//     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-  return (
-    <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Portfolio page of r134x7 aka ggx2ac"
-        />
-      </Head>
-        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-                <div>
-                    <Header  />
-                    <main>{children}</main>
-                    <Footer />
-                </div>
-            </MantineProvider>
-        </ColorSchemeProvider>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Head>
+//         <link rel="icon" href="/favicon.ico" />
+//         <meta
+//           name="description"
+//           content="Portfolio page of r134x7 aka ggx2ac"
+//         />
+//       </Head>
+//         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+//             <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+//                 <div>
+//                     <Header  />
+//                     <main>{children}</main>
+//                     <Footer />
+//                 </div>
+//             </MantineProvider>
+//         </ColorSchemeProvider>
+//     </div>
+//   );
+// }
+export {}
