35da6f619ac355fa6c03ab2ed72f00e5f95c5856
r134x7 <r134x7@gmx.com>
2023-05-01 03:22:04 +1000

    updated portfolio due to migration away from HEROKU!

--- vite-refactor/src/components/project.tsx
+++ vite-refactor/src/components/project.tsx
@@ -1,6 +1,6 @@
 import my2 from "../images/my2.jpg";
 import my3 from "../images/tech2.jpg";
-import my7 from "../images/my7.jpg";
+import my7 from "../images/my9.jpg";
 import my8 from "../images/my8.jpg";
 import my9 from "../images/psmk2a1.jpg";
 import my10 from "../images/my10.jpg";
@@ -21,7 +21,7 @@ export const works = [
     {
         id: 2,
         name: "Project S.C.A.L.E.",
-        deployedApp: "https://ambitions-scale-up.herokuapp.com/",
+        deployedApp: "https://ambitions-scale-up.onrender.com/",
         githubRepo: "https://github.com/r134x7/project-scale",
         image: my8,
         badge: "TypeScript",
@@ -45,7 +45,7 @@ export const works = [
     {
         id: 4,
         name: "GraphQL Book Search",
-        deployedApp: "https://intense-everglades-58353.herokuapp.com/",
+        deployedApp: "https://graphql-book-search.onrender.com/",
         githubRepo: "https://github.com/r134x7/graphql-book-search",
         image: my7,
         badge: "JavaScript",
@@ -55,30 +55,42 @@ export const works = [
         textDescription: "A React app Book Search \"engine\" using Google Books API to handle GET requests when searching for book titles. Apollo GraphQL is used for handling user data i.e. signing/logging a user, saving the list of books and removing books from their saved list. JWT authentication is used when signing or logging in a user. Apollo GraphQL saved books are cached to make it quicker to remove books from your list as opposed to making network calls to the database. The database used is MongoDB."
     },
     {
-        id: 5,
-        name: "RunningTracks",
-        deployedApp: "https://quiet-reaches-87273.herokuapp.com/",
-        githubRepo: "https://github.com/r134x7/RunningTracks",
-        image: my2,
+        id: 7,
+        name: "Infernal Redux Toolkit Shop",
+        deployedApp: "https://graphql-book-search.onrender.com/",
+        githubRepo: "https://github.com/r134x7/infernal-redux-toolkit-shop",
+        image: my7,
         badge: "JavaScript",
         badgeColour: "yellow",
         buttonAppText: "Click here to Deployed App",
         buttonRepoText: "Click here to GitHub Repo",
-        textDescription: "This is a full-stack application with the MVC model where Handlebars is used for the view, MySQL is used for the model with Sequelize as the ORM and Express is used as the controller for routing all the CRUD operations. The app takes in data input from users who are fitness runners and displays their data in bar charts. Users who post their runs can have other users comment on the runs."
-    },
-    {
-        id: 6,
-        name: "Tech News Java API",
-        deployedApp: "https://rocky-waters-09134.herokuapp.com/",
-        githubRepo: "https://github.com/r134x7/tech-news-java-api",
-        image: my3,
-        badge: "Java",
-        badgeColour: "orange",
-        buttonAppText: "Click here to Deployed App",
-        buttonRepoText: "Click here to GitHub Repo",
-        textDescription: "A tech news blog API built on Java front and back-end frameworks. This project is mainly focused on building the back-end and API to handle CRUD requests using Java. You create an account to post links to various tech sites for which people can make comments on the post made, all posts appear on the homepage. This project uses the MVC layout with Spring Boot for the back-end, MySQL for the database and, Thymeleaf for the front-end."
+        textDescription: "A React app online shop using Redux Toolkit to handle the state and state changes throughout the app, react-redux is used specifically for useSelector and useDispatch redux functions in a react app. The app was refactored from using React's Context API to using Redux Toolkit. The app uses MongoDB for the database and mutations and queries for the database are handled by Apollo GraphQL. The online shop uses the Stripe API payment system but no actual payments are made as it is a test system. Purchases made on the app will appear in the order history of your account. JWT authentication is used when signing or logging in a user."
+    }, 
+    // {
+    //     id: 5,
+    //     name: "RunningTracks",
+    //     deployedApp: "https://quiet-reaches-87273.herokuapp.com/",
+    //     githubRepo: "https://github.com/r134x7/RunningTracks",
+    //     image: my2,
+    //     badge: "JavaScript",
+    //     badgeColour: "yellow",
+    //     buttonAppText: "Click here to Deployed App",
+    //     buttonRepoText: "Click here to GitHub Repo",
+    //     textDescription: "This is a full-stack application with the MVC model where Handlebars is used for the view, MySQL is used for the model with Sequelize as the ORM and Express is used as the controller for routing all the CRUD operations. The app takes in data input from users who are fitness runners and displays their data in bar charts. Users who post their runs can have other users comment on the runs."
+    // },
+    // {
+    //     id: 6,
+    //     name: "Tech News Java API",
+    //     deployedApp: "https://rocky-waters-09134.herokuapp.com/",
+    //     githubRepo: "https://github.com/r134x7/tech-news-java-api",
+    //     image: my3,
+    //     badge: "Java",
+    //     badgeColour: "orange",
+    //     buttonAppText: "Click here to Deployed App",
+    //     buttonRepoText: "Click here to GitHub Repo",
+    //     textDescription: "A tech news blog API built on Java front and back-end frameworks. This project is mainly focused on building the back-end and API to handle CRUD requests using Java. You create an account to post links to various tech sites for which people can make comments on the post made, all posts appear on the homepage. This project uses the MVC layout with Spring Boot for the back-end, MySQL for the database and, Thymeleaf for the front-end."
 
-    },
+    // },
 ]
 
 // function Project() { // exporting the props to Project.js
